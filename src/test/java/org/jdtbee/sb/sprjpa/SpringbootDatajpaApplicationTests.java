be7a39a0aa39743df7f2ddd9663225068bcae400
Thomas Immen <thimmen@mail.de>
2024-01-30 21:58:28 +0100

    setup project

---
+++ src/test/java/org/jdtbee/sb/sprjpa/SpringbootDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jdtbee.sb.sprjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
