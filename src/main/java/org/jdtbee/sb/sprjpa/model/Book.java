9caf93fc58777b8cccc1e950048ea8f117cec6d6
Thomas Immen <thimmen@mail.de>
2024-02-01 15:56:45 +0100

    test und aenderung an der manytoone konfiguration (joincolumn) der foreign key eintrag hatte voerst keine funktion (?)

--- src/main/java/org/jdtbee/sb/sprjpa/model/Book.java
+++ src/main/java/org/jdtbee/sb/sprjpa/model/Book.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,8 +37,9 @@ public class Book {
   private LocalDateTime createdAt;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "member_id", nullable = false, referencedColumnName = "id",
-      foreignKey = @ForeignKey(name = "member_book_fk"))
+  // @JoinColumn(name = "member_id", nullable = false, referencedColumnName = "id",
+  // foreignKey = @ForeignKey(name = "member_book_fk"))
+  @JoinColumn(name = "member_id", nullable = false, referencedColumnName = "id")
   private Member member;
 
   @Override
